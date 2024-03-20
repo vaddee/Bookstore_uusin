ae3fa5c8db2f52eb1cbdebab4f20eb5183ddbdb8
vaddee <valtteri.laakso@hotmail.fi>
2024-03-20 19:23:50 +0200

    smoke
    lisatty viela smoke testit. Kaikki testit onnistuu

--- bookstore/src/test/java/fi/hh/bookstore/BookstoreApplicationTests.java
+++ bookstore/src/test/java/fi/hh/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,42 @@
 package fi.hh.bookstore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import fi.hh.bookstore.web.BookController;
+import fi.hh.bookstore.web.CategoryController;
+
 
 @SpringBootTest
 class BookstoreApplicationTests {
 
+	@Autowired
+		private BookController bookController;
+
+		@Autowired
+		private CategoryController categoryController;
+
 	@Test
-	void contextLoads() {
+	void bookControllerTest() {
+		
+
+		assertThat(bookController).isNotNull();
+
 	}
+
+	@Test
+	void categoryControllerTest() {
+		
+
+		assertThat(categoryController).isNotNull();
+
+	}
+
+
 
 }
